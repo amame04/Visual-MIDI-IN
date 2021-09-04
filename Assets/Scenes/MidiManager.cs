5241d2fc652bcf09066704df0885a2b91fbcf63f
amame04 <ameame2t@gmail.com>
2021-09-04 16:41:31 +0900

    Making efficient

--- Assets/Scenes/MidiManager.cs
+++ Assets/Scenes/MidiManager.cs
@@ -27,36 +27,48 @@ public class MidiManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // If you press Esc, it show the menu
         if (Input.GetKey(KeyCode.Escape))
         {
-            SetPanelActive();
+            ShowSettingMenu();
         }
+
+        // Monitor changes of MidiOnOff and MidiVelocityArray
         for (int i = 0; i < MidiOnOff.Length; i++)
         {
             if(MidiOnOff[i] == true)
             {
                 if (MidiVelocityArray[i] != 0)
-                NoteOn(i, MidiVelocityArray[i]);
+                {
+                    // Start animation
+                    NoteOn(i, MidiVelocityArray[i]);
+                }
             }
             else
             {
+                // Stop animation
                 NoteOff(i);
             }
         }
     }
     private void OnEnable()
     {
+        // Start MIDI port
         StartMidiInPort(0);
         StartMidiOutPort(0);
-        SetPanelActive();
+
+        // Show menu
+        ShowSettingMenu();
     }
 
     private void OnDisable()
     {
+        // Stop MIDI port
         StopMidiInPort();
         StopMidiOutPort();
     }
 
+    // Lock MIDI input device
     private void StartMidiInPort(int DevNumber)
     {
         MidiInPort = new MidiIn(DevNumber);
@@ -65,11 +77,13 @@ public class MidiManager : MonoBehaviour
         MidiInPort.Start();
     }
     
+    // Lock MIDI output device
     private void StartMidiOutPort(int DevNumber)
     {
         MidiOutPort = new MidiOut(DevNumber);
     }
     
+    // Release MIDI input device
     private void StopMidiInPort()
     {
         MidiInPort.Stop();
@@ -78,16 +92,19 @@ public class MidiManager : MonoBehaviour
         MidiInPort.ErrorReceived -= MidiInErrorReceived;
     }
     
+    // Release MIDI output device
     private void StopMidiOutPort()
     {
         MidiOutPort.Dispose();
     }
 
+    // If it received MIDI input error, it output logs (Finally, it do nothing)
     void MidiInErrorReceived(object sender, MidiInMessageEventArgs e)
     {
         Debug.Log("Error Received");
     }
 
+    // If it recieved MIDI message, it send Raw message to MIDI output and Start animation
     void MidiInMessageReceived(object sender, MidiInMessageEventArgs e)
     {
         MidiOutPort.Send(e.RawMessage);
@@ -96,30 +113,51 @@ public class MidiManager : MonoBehaviour
         int MidiNote = (e.RawMessage & 0xff00) >> 8;
         int MidiEvent = e.RawMessage & 0x000000ff;
 
+        // If MidiEvent == Note on
         if (MidiEvent == 0x90 && MidiVelocity != 0)
         {
             MidiOnOff[MidiNote] = true;
             MidiVelocityArray[MidiNote] = MidiVelocity;
-        } else if ((MidiEvent == 0x90 && MidiVelocity == 0) || (MidiEvent == 0x80))
+        }
+        // If MidiEvent == Note Off
+        else if ((MidiEvent == 0x90 && MidiVelocity == 0) || (MidiEvent == 0x80))
         {
             MidiOnOff[MidiNote] = false;
         }
     }
 
+    // Change key color and play animation
     private void NoteOn(int MidiNote, int Velocity)
     {
         string NoteName = ReturnNoteName(MidiNote);
         GameObject CurNote = GameObject.Find(NoteName);
-        CurNote.GetComponent<Animator>().SetBool("midiOn", true);
+        //CurNote.GetComponent<Animator>().SetBool("midiOn", true);
+
+        CurNote.GetComponent<Renderer>().material.color = Color.green;
 
         PlayFireWorks(MidiNote, Velocity);
     }
     
+    // Revert key color
     private void NoteOff(int MidiNote)
     {
         string NoteName = ReturnNoteName(MidiNote);
         GameObject CurNote = GameObject.Find(NoteName);
-        CurNote.GetComponent<Animator>().SetBool("midiOn", false);
+        //CurNote.GetComponent<Animator>().SetBool("midiOn", false);
+        
+        switch (MidiNote % 12)
+        {
+            case 1:
+            case 3:
+            case 6:
+            case 8:
+            case 10:
+                CurNote.GetComponent<Renderer>().material.color = Color.black;
+                break;
+            default:
+                CurNote.GetComponent<Renderer>().material.color = Color.white;
+                break;
+        }
     }
 
     private string ReturnNoteName (int MidiNote)
@@ -135,6 +173,7 @@ public class MidiManager : MonoBehaviour
         var FireWorksPar = FireWorksObj.GetComponent<ParticleSystem>();
         var FireWorksParTr = FireWorksPar.trails;
 
+        // Create ramdom gradation
         var FireWorksGrad = new Gradient();
 
         var FireWorksGradCol = new GradientColorKey[2];
@@ -150,8 +189,11 @@ public class MidiManager : MonoBehaviour
         FireWorksGradAlf[1].time = 1.0f;
 
         FireWorksGrad.SetKeys(FireWorksGradCol, FireWorksGradAlf);
+
+        // Apply the gradation
         FireWorksParTr.colorOverTrail = FireWorksGrad;
         
+        // Play FireWorks
         if (!FireWorksPar.isPlaying)
         {
         float FireWorksScale = Velocity / 7.0f;
@@ -161,15 +203,19 @@ public class MidiManager : MonoBehaviour
         MidiVelocityArray[MidiNote] = 0;
     }
 
-    public void SetPanelActive()
+    public void ShowSettingMenu()
     {
         SetPanel.SetActive(true);
+
+        // Enumelate midi device
         ListingMidiDev();
 
+        // Set cursor the device that you're using
         MidiInDrp.value = CurMidiDev[0];
         MidiOutDrp.value = CurMidiDev[1];
     }
 
+    // Apply changes and hide menu
     public void PushOkBtn()
     {
         StopMidiInPort();
@@ -177,19 +223,23 @@ public class MidiManager : MonoBehaviour
         StartMidiInPort(MidiInDrp.value);
         StartMidiOutPort(MidiOutDrp.value);
 
+        // Change current midi device
         CurMidiDev[0] = MidiInDrp.value;
         CurMidiDev[1] = MidiOutDrp.value;
 
         SetPanel.SetActive(false);
     }
 
+    // Hide menu (do nothing)
     public void PushCancelBtn()
     {
         SetPanel.SetActive(false);
     }
 
+    // Enumelate and list available midi devices
     public void ListingMidiDev()
     {
+        // List midi devices
         var MidiInList = new List<string>();
         var MidiOutList = new List<string>();
 
@@ -203,9 +253,11 @@ public class MidiManager : MonoBehaviour
             MidiOutList.Add(MidiOut.DeviceInfo(device).ProductName);
         }
 
+        // Clear lsit
         MidiInDrp.ClearOptions();
         MidiOutDrp.ClearOptions();
 
+        // Set list
         MidiInDrp.AddOptions(MidiInList);
         MidiOutDrp.AddOptions(MidiOutList);
     }
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-Visual-MIDI-IN
-==============
+# Visual-MIDI-IN
 ![sample](sample.png)
 
 At the moment it only supports fireworks, but I plan to add more effects in the future.
+
+# FAQ
+* Q. How do I use VSTi
+  * A. Please use [loopMIDI(Tobias Erichsen)](https://www.tobias-erichsen.de/software/loopmidi.html) 
